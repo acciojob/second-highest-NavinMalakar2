eadbf12b27af9b8ea197cd12c94a308b3ac3f400
Navin Malakar <161839292+NavinMalakar2@users.noreply.github.com>
2025-07-10 10:55:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function secondHighest(arr) {
+  if (arr.length < 2) return -Infinity;
+
   let max = -Infinity;
   let second = -Infinity;
 
   for (let num of arr) {
     if (num > max) {
-      if (num !== max) 
-		  second = max; 
-	      max = num; 
+      second = max;
+      max = num;
     } else if (num > second && num < max) {
       second = num;
     }
+  }
+
+  // If second never updated => all values were equal or only one unique value
+  if (second === -Infinity) {
+    return -Infinity;
   }
 
   return second;
